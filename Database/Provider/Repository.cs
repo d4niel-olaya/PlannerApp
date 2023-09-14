04ec6855d864ddaf1d571a0f30b80acb07a9fe73
d4niel <olayajuandaniel8@gmail.com>
2023-09-13 22:10:16 -0500

    abstract

--- Database/Provider/Repository.cs
+++ Database/Provider/Repository.cs
@@ -1,6 +1,15 @@
 
 
-public abstract class Repository
+using PlannerApp.Database;
+
+public abstract class Repository<TDbService,TModel> where TDbService : IDb             
 {
+    public abstract Task<IEnumerable<TModel>> GetAsync();
+
+    public abstract Task<TModel> CreateAsync(TModel model);
+
+    public abstract Task<TModel> UpdateAsync(TModel model);
+
     
-}
+
+}        
