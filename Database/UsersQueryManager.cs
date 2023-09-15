ad837e577232293553ec1ba7cbb396a4588ace19
d4niel <olayajuandaniel8@gmail.com>
2023-09-14 21:49:15 -0500

    cmd

--- Database/UsersQueryManager.cs
+++ Database/UsersQueryManager.cs
@@ -1,5 +1,7 @@
 
 
+using PlannerApp.Database.Models;
+
 namespace PlannerApp.Database;
 
 public class UserQueryManager 
@@ -9,6 +11,22 @@ public class UserQueryManager
     public UserQueryManager(IDb dbservice)
     {
         _dbservice  =   dbservice;
+    }
+
+    public async Task Register(User user)
+    {
+        await _dbservice.OpenDb();
+        using(var cmd = _dbservice.GetCommand())
+        {
+            cmd.Connection = _dbservice.GetProvider();
+            cmd.CommandText = "INSERT INTO Users(UserNickname, UserEmail, UserPassword, UserRole) VALUES(@N, @E, @P, @R)";
+            cmd.Parameters.AddWithValue("@N",user.UserNickname);
+            cmd.Parameters.AddWithValue("@E",user.UserEmail);
+            cmd.Parameters.AddWithValue("@P",user.UserPassword);
+            cmd.Parameters.AddWithValue("@R",user.UserRole);
+            await cmd.ExecuteNonQueryAsync();
+        }
+        await _dbservice.CloseDb();
     }
 }
 
