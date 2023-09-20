4af33a6eaee5a195ab110e6026a89f9b90713d4d
d4niel <olayajuandaniel8@gmail.com>
2023-09-19 21:57:49 -0500

    pj repository

--- Database/ProjectsRepository.cs
+++ Database/ProjectsRepository.cs
@@ -7,9 +7,31 @@ namespace PlannerApp.Database.Repository;
 
 public class ProjectsRepository : Repository<IDb, Project>
 {
-    public override Task<Project> CreateAsync(Project model)
+    private readonly IDb _dbService;
+
+
+    public ProjectsRepository(IDb db)
     {
-        throw new NotImplementedException();
+        _dbService = db;
+    }
+
+    public override async Task<Project> CreateAsync(Project model)
+    {
+        await _dbService.OpenDb();
+        using(var cmd = _dbService.GetCommand())
+        {
+            cmd.Connection = _dbService.GetProvider();
+            cmd.CommandText = "INSERT INTO Projects(ProjectName, ProjectDescription) VALUES(@N, @D)";
+            cmd.Parameters.AddWithValue("@N",model.ProjectName);
+            cmd.Parameters.AddWithValue("@D",model.ProjectDescription);
+           // cmd.Parameters.AddWithValue("@P",user.UserPassword);
+            //cmd.Parameters.AddWithValue("@R",user.UserRole);
+            await cmd.ExecuteNonQueryAsync();
+            
+        }
+        await _dbService.CloseDb();
+        return new Project{ ProjectName = model.ProjectName, ProjectDescription = model.ProjectDescription};
+
     }
 
     public override Task<IEnumerable<Project>> GetAsync()
