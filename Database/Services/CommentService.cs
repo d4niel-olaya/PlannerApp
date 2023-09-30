1c5f381e910e391a5d13debc3bde6c068039839e
d4niel <olayajuandaniel8@gmail.com>
2023-09-29 21:39:17 -0500

    comments service

---
+++ Database/Services/CommentService.cs
@@ -0,0 +1,40 @@
+using PlannerApp.Database.Models;
+using PlannerApp.Database.Repository;
+
+namespace PlannerApp.Database.Services;
+
+public class CommentService : ICommentService
+{
+
+     private readonly CommentsRepository _commentsRepository;
+
+    public CommentService(CommentsRepository commentsRepository)
+    {
+         _commentsRepository = commentsRepository;
+    }
+    public async Task<Comments> CreateComment(Comments model)
+    {
+        var comment = await _commentsRepository.CreateAsync(model);
+        return comment;
+    }
+
+    public async Task<List<Comments>> GetComments()
+    {
+        var commentsList = await _commentsRepository.GetAsync();
+        return commentsList;
+    }
+
+    public void SetIdTask(int id)
+    {
+        _commentsRepository.SetTaskId(id);
+    }
+}
+
+public interface ICommentService
+{
+    Task<Comments> CreateComment(Comments model);
+    Task<List<Comments>> GetComments();
+
+    void SetIdTask(int id);
+    
+}
