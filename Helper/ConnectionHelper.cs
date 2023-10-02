f5e983f68b6ce3c5aae460aa490c702df8455c10
d4niel <olayajuandaniel8@gmail.com>
2023-10-01 21:37:26 -0500

    api url

--- Helper/ConnectionHelper.cs
+++ Helper/ConnectionHelper.cs
@@ -7,4 +7,11 @@ public class DataHelper
         var productionEnviroment = Environment.GetEnvironmentVariable("DATABASE_URL");
         return string.IsNullOrEmpty(productionEnviroment) ? devEnviroment : productionEnviroment;
     }
+
+    public static string GetApiUrl(IConfiguration configuration)
+    {
+        var devEnviroment = configuration["api_url"];
+        var productionEnviroment = Environment.GetEnvironmentVariable("API_URL");
+        return string.IsNullOrEmpty(productionEnviroment) ? devEnviroment : productionEnviroment;
+    }
 }
